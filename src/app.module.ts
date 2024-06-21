aecdcee1aba55f585e9a592558a7dc28ab10533f
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2024-06-21 10:36:51 +0200

    docker postgres initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER || 'admin',
+      password: process.env.DATABASE_PASSWORD || 'admin',
+      database: process.env.DATABASE_NAME || 'mydatabase',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
